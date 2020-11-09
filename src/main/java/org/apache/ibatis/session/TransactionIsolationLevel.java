df920b5ee14ddfbc82e2bd87d90e643990a440fe
zhouhang <zhouhang01@baijiahulian.com>
2020-11-09 16:19:39 +0800

    [docs]: 增加注释

--- src/main/java/org/apache/ibatis/session/TransactionIsolationLevel.java
+++ src/main/java/org/apache/ibatis/session/TransactionIsolationLevel.java
@@ -25,11 +25,25 @@ import java.sql.Connection;
  * 
  */
 public enum TransactionIsolationLevel {
-	//包括JDBC支持的5个级别
+  /**
+   * 不隔离
+   */
   NONE(Connection.TRANSACTION_NONE),
+  /**
+   * 读已提交
+   */
   READ_COMMITTED(Connection.TRANSACTION_READ_COMMITTED),
+  /**
+   * 读未提交
+   */
   READ_UNCOMMITTED(Connection.TRANSACTION_READ_UNCOMMITTED),
+  /**
+   * 可重复读
+   */
   REPEATABLE_READ(Connection.TRANSACTION_REPEATABLE_READ),
+  /**
+   * 串行化
+   */
   SERIALIZABLE(Connection.TRANSACTION_SERIALIZABLE);
 
   private final int level;
